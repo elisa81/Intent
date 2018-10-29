5a4d855a69c5a7aa96254fb98b31a75ab60e9bfe
Elisa <elisacho81@gmail.com>
2018-10-29 14:51:26 +0900

    Initial commit

---
+++ app/src/main/java/com/jmj/edu/intent/MainActivity.java
@@ -0,0 +1,33 @@
+package com.jmj.edu.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1, button2, button3;
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = null;
+        intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
+    }
+}
