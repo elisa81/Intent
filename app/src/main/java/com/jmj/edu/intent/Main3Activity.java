eeebb3d946e33d25ebd848baa99b0ba590514ba8
Elisa <elisacho81@gmail.com>
2018-10-30 10:03:50 +0900

    Main Page to Page 2 to Page 3

--- app/src/main/java/com/jmj/edu/intent/Main3Activity.java
+++ app/src/main/java/com/jmj/edu/intent/Main3Activity.java
@@ -1,13 +1,31 @@
 package com.jmj.edu.intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main3Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String text = ((Button) v).getText().toString();
+        Intent intent = null;
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/jmj/edu/intent/MainActivity.java
+++ app/src/main/java/com/jmj/edu/intent/MainActivity.java
@@ -8,36 +8,25 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1, button2, button3;
         button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
         button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
 
         String text = ((Button) v).getText().toString();
-        int id = v.getId();
         Intent intent = null;
+        intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
 
-            switch(id) {
-            case R.id.button1:
-                intent = new Intent(this, Main2Activity.class);
-                startActivity(intent);
-                break;
-                case R.id.button2:
-                    intent = new Intent(this, Main3Activity.class);
-                    startActivity(intent);
-                    break;
-        }
     }
 }
